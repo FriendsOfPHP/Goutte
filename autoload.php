09f00f42b70450d655e39b4eac070a5922aeb4b1
Igor Wiedler <igor@wiedler.ch>
2011-08-14 20:57:39 +0200

    Make the require_once of the UnviversalClassLoader conditional

--- autoload.php
+++ autoload.php
@@ -1,6 +1,8 @@
 <?php
 
-require_once __DIR__.'/vendor/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+if (false === class_exists('Symfony\Component\ClassLoader\UniversalClassLoader', false)) {
+    require_once __DIR__.'/vendor/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+}
 
 use Symfony\Component\ClassLoader\UniversalClassLoader;
 
