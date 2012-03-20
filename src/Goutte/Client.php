415d700128e92cd9a7923a89b1e3779cc0534ce9
Christian Eikermann <christian@chrisdev.de>
2012-03-20 08:40:02 +0100

    Improved the client for using Buzz

--- src/Goutte/Client.php
+++ src/Goutte/Client.php
@@ -8,8 +8,10 @@ use Symfony\Component\BrowserKit\CookieJar;
 use Symfony\Component\BrowserKit\Request;
 use Symfony\Component\BrowserKit\Response;
 
-use Zend\Http\Client as ZendClient;
-use Zend\Http\Response as ZendResponse;
+use Buzz\Browser as BuzzBrowser;
+use Buzz\Client\Curl as BuzzCurlAdapter;
+use Buzz\Message\Request as BuzzRequest;
+use Buzz\Message\Response as BuzzResponse;
 
 /*
  * This file is part of the Goutte package.
@@ -25,115 +27,56 @@ use Zend\Http\Response as ZendResponse;
  *
  * @package Goutte
  * @author  Fabien Potencier <fabien.potencier@symfony-project.com>
+ * @author  Christian Eikermann <christian@chrisdev.de>
  */
 class Client extends BaseClient
 {
-    const VERSION = '0.1';
-
-    protected $zendConfig;
-    protected $headers = array();
-    protected $auth = null;
-
-    public function __construct(array $zendConfig = array(), array $server = array(), History $history = null, CookieJar $cookieJar = null)
-    {
-        $this->zendConfig = $zendConfig;
-
-        parent::__construct($server, $history, $cookieJar);
-    }
-
-    public function setHeader($name, $value)
-    {
-        $this->headers[$name] = $value;
-    }
-
-    public function setAuth($user, $password = '', $type = ZendClient::AUTH_BASIC)
-    {
-        $this->auth = array(
-            'user'     => $user,
-            'password' => $password,
-            'type'     => $type
-        );
-    }
+    protected $browser = null;
 
     protected function doRequest($request)
     {
-        $client = $this->createClient($request);
-
-        $response = $client->send($client->getRequest());
-
-        return $this->createResponse($response);
+        $buzzRequest = $this->createRequest($request);
+        
+        $buzzResponse = $this->getBrowser()->send($buzzRequest);
+        
+        return $this->createResponse($buzzResponse);
     }
 
-    protected function createClient(Request $request)
+    protected function createRequest(Request $request)
     {
-        $client = $this->createZendClient();
-        $client->setUri($request->getUri());
-        $client->setConfig(array_merge(array(
-            'maxredirects' => 0,
-            'timeout'      => 30,
-            'useragent'    => $this->server['HTTP_USER_AGENT'],
-            'adapter'      => 'Zend\\Http\\Client\\Adapter\\Socket',
-            ), $this->zendConfig));
-        $client->setMethod(strtoupper($request->getMethod()));
-
-        if ($request->getContent() !== null) {
-            $client->setRawBody($request->getContent());
-        }
-
-        if ('POST' == $request->getMethod()) {
-            $client->setParameterPost($request->getParameters());
-        }
-        $client->setHeaders($this->headers);
-
-        if ($this->auth !== null) {
-            $client->setAuth(
-                $this->auth['user'],
-                $this->auth['password'],
-                $this->auth['type']
-            );
-        }
-
-        foreach ($this->getCookieJar()->allValues($request->getUri()) as $name => $value) {
-            $client->addCookie($name, $value);
-        }
-
-        $this->addFileUploadsRecursively($client, $request->getFiles());
-
-        return $client;
-    }
-
-    /**
-     * Goes recursively through the files array and adds uploads to the ZendClient
-     */
-    protected function addFileUploadsRecursively(ZendClient $client, array $files, $arrayName = '')
-    {
-        foreach ($files as $name => $info) {
-            if (!empty($arrayName)) {
-                $name = $arrayName . '[' . $name . ']';
+        $buzzRequest = new BuzzRequest();
+        $buzzRequest->setMethod($request->getMethod());
+        $buzzRequest->fromUrl($request->getUri());
+        $buzzRequest->addHeaders($request->getServer());
+        $buzzRequest->setContent($request->getContent());
+        
+        if ($request->getMethod() == 'POST') {
+            $content = http_build_query($request->getParameters());
+            $buzzRequest->setContent($content);
+        } else {
+            $url = $request->getUri();
+            $parameters = $request->getParameters();
+            if (count($parameters) > 0) {
+                $url .= '?'.http_build_query($request->getParameters());
             }
-            if (isset($info['tmp_name']) && isset($info['name'])) {
-                if ('' !== $info['tmp_name'] && '' !== $info['name']) {
-                    $filename = $info['name'];
-
-                    if (false === ($data = @file_get_contents($info['tmp_name']))) {
-                        throw new \RuntimeException("Unable to read file '{$filename}' for upload");
-                    }
-
-                    $client->setFileUpload($filename, $name, $data);
-                }
-            } elseif (is_array($info)) {
-                $this->addFileUploadsRecursively($client, $info, $name);
-            }
+            
+            $buzzRequest->fromUrl($url);
         }
+        
+        return $buzzRequest;
     }
 
-    protected function createResponse(ZendResponse $response)
+    protected function createResponse(BuzzResponse $response)
     {
-        return new Response($response->getBody(), $response->getStatusCode(), $response->headers()->toArray());
+        return new Response($response->getContent(), $response->getStatusCode(), $response->getHeaders());
     }
-
-    protected function createZendClient()
+    
+    public function getBrowser()
     {
-        return new ZendClient(null, array('encodecookies' => false));
+        if (!$this->browser) {
+            $this->browser = new BuzzBrowser(new BuzzCurlAdapter());
+        }
+        
+        return $this->browser;
     }
 }
