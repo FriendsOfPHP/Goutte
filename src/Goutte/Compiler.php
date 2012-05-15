96c8999ec7fead53bc0e3834dbd6d2c38dde4b8f
Fabien Potencier <fabien.potencier@gmail.com>
2012-05-15 10:22:28 +0200

    fixed CS

--- src/Goutte/Compiler.php
+++ src/Goutte/Compiler.php
@@ -1,9 +1,5 @@
 <?php
 
-namespace Goutte;
-
-use Symfony\Component\Finder\Finder;
-
 /*
  * This file is part of the Goutte utility.
  *
@@ -13,58 +9,59 @@ use Symfony\Component\Finder\Finder;
  * with this source code in the file LICENSE.
  */
 
+namespace Goutte;
+
+use Symfony\Component\Finder\Finder;
+
 /**
  * The Compiler class compiles the Goutte utility.
  *
- * @package    Goutte
- * @author     Fabien Potencier <fabien@symfony.com>
+ * @author Fabien Potencier <fabien@symfony.com>
  */
 class Compiler
 {
-  public function compile($pharFile = 'goutte.phar')
-  {
-    if (file_exists($pharFile))
+    public function compile($pharFile = 'goutte.phar')
     {
-      unlink($pharFile);
+        if (file_exists($pharFile)) {
+            unlink($pharFile);
+        }
+
+        $phar = new \Phar($pharFile, 0, 'Goutte');
+        $phar->setSignatureAlgorithm(\Phar::SHA1);
+
+        $phar->startBuffering();
+
+        // CLI Component files
+        foreach ($this->getFiles() as $file) {
+            $path = str_replace(__DIR__.'/', '', $file);
+            $phar->addFromString($path, file_get_contents($file));
+        }
+
+        // Stubs
+        $phar['_cli_stub.php'] = $this->getCliStub();
+        $phar['_web_stub.php'] = $this->getWebStub();
+        $phar->setDefaultStub('_cli_stub.php', '_web_stub.php');
+
+        $phar->stopBuffering();
+
+        // $phar->compressFiles(\Phar::GZ);
+
+        unset($phar);
     }
 
-    $phar = new \Phar($pharFile, 0, 'Goutte');
-    $phar->setSignatureAlgorithm(\Phar::SHA1);
-
-    $phar->startBuffering();
-
-    // CLI Component files
-    foreach ($this->getFiles() as $file)
+    protected function getCliStub()
     {
-      $path = str_replace(__DIR__.'/', '', $file);
-      $phar->addFromString($path, file_get_contents($file));
+        return "<?php ".$this->getLicense()." require_once __DIR__.'/vendor/autoload.php'; __HALT_COMPILER();";
     }
 
-    // Stubs
-    $phar['_cli_stub.php'] = $this->getCliStub();
-    $phar['_web_stub.php'] = $this->getWebStub();
-    $phar->setDefaultStub('_cli_stub.php', '_web_stub.php');
+    protected function getWebStub()
+    {
+        return "<?php throw new \LogicException('This PHAR file can only be used from the CLI.'); __HALT_COMPILER();";
+    }
 
-    $phar->stopBuffering();
-
-    // $phar->compressFiles(\Phar::GZ);
-
-    unset($phar);
-  }
-
-  protected function getCliStub()
-  {
-    return "<?php ".$this->getLicense()." require_once __DIR__.'/vendor/autoload.php'; __HALT_COMPILER();";
-  }
-
-  protected function getWebStub()
-  {
-    return "<?php throw new \LogicException('This PHAR file can only be used from the CLI.'); __HALT_COMPILER();";
-  }
-
-  protected function getLicense()
-  {
-    return '
+    protected function getLicense()
+    {
+        return '
     /*
      * This file is part of the Goutte utility.
      *
@@ -73,30 +70,29 @@ class Compiler
      * This source file is subject to the MIT license that is bundled
      * with this source code in the file LICENSE.
      */';
-  }
+    }
 
-  protected function getFiles()
-  {
-    $files = array(
-      'LICENSE',
-      'vendor/autoload.php',
-    );
+    protected function getFiles()
+    {
+        $files = array(
+            'LICENSE',
+            'vendor/autoload.php',
+        );
 
-    $dirs = array(
-      'vendor/composer',
-      'src/Goutte',
-      'vendor/symfony/browser-kit',
-      'vendor/symfony/dom-crawler',
-      'vendor/symfony/css-selector',
-      'vendor/symfony/process',
-      'vendor/symfony/event-dispatcher',
-      'vendor/guzzle/guzzle/src/Guzzle'
-    );
+        $dirs = array(
+            'vendor/composer',
+            'src/Goutte',
+            'vendor/symfony/browser-kit',
+            'vendor/symfony/dom-crawler',
+            'vendor/symfony/css-selector',
+            'vendor/symfony/process',
+            'vendor/symfony/event-dispatcher',
+            'vendor/guzzle/guzzle/src/Guzzle'
+        );
 
-    $finder = new Finder();
-    $iterator = $finder->files()->name('*.php')->in($dirs);
+        $finder = new Finder();
+        $iterator = $finder->files()->name('*.php')->in($dirs);
 
-    return array_merge($files, iterator_to_array($iterator));
-  }
+        return array_merge($files, iterator_to_array($iterator));
+    }
 }
-
