84da2bbada999972ae7503b0c535ae833af27ac6
Fabien Potencier <fabien@potencier.org>
2023-04-01 09:04:07 +0200

    Add some deprecation notice to encourage people to use HttpBrowser instead from the Symfony BrowserKit component

--- Goutte/Client.php
+++ Goutte/Client.php
@@ -11,11 +11,22 @@
 
 namespace Goutte;
 
+use Symfony\Component\BrowserKit\CookieJar;
+use Symfony\Component\BrowserKit\History;
 use Symfony\Component\BrowserKit\HttpBrowser;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 /**
  * @author Fabien Potencier <fabien@symfony.com>
+ *
+ * @deprecated Use Symfony\Component\BrowserKit\HttpBrowser directly instead
  */
 class Client extends HttpBrowser
 {
+    public function __construct(HttpClientInterface $client = null, History $history = null, CookieJar $cookieJar = null)
+    {
+        trigger_deprecation('fabpot/goutte', '4.0', 'The "%s" class is deprecated, use "%s" instead.', __CLASS__, HttpBrowser::class);
+
+        parent::__construct($client, $history, $cookieJar);
+    }
 }
